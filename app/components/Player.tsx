4ad3f0a01bc0cd365f4499414278f3b47629eed3
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-08 22:00:30 +0200

    feat: basic types, parsing

--- app/components/Player.tsx
+++ app/components/Player.tsx
@@ -9,14 +9,12 @@ const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 const Player = ({ id }: IPlayer) => {
   return id ? (
-    <>
-      <ReactPlayer
-        url={`/items/${id}/video.mp4`}
-        loop={true}
-        playing
-        controls={true}
-      />
-    </>
+    <ReactPlayer
+      url={`/items/${id}/video.mp4`}
+      loop={true}
+      playing
+      controls={true}
+    />
   ) : null;
 };
 export default Player;
--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,12 +1,44 @@
-import Player from "../components/Player";
+import fs from "fs";
+import path from "path";
+import { Item } from "@/app/components/types";
+import Player from "@/app/components/Player";
 
-const Page = async () => {
-  const x = 1;
+async function getData(): Promise<{ count: number; items: Item[] }> {
+  const itemFolder = path.join(process.cwd(), "public", "items");
+  const items = fs.readdirSync(itemFolder);
+
+  return {
+    count: items.length,
+    items: items.map((strId) => {
+      const itemDataJson = `${itemFolder}/${strId}/data.json`;
+      let title = "unknown";
+      if (fs.existsSync(itemDataJson)) {
+        // OPTIM: possible to make this parallel
+        const metadataContent = fs.readFileSync(itemDataJson, "utf8");
+        const metadata: Omit<Item, "id"> = JSON.parse(metadataContent);
+        title = metadata.title;
+      }
+      return {
+        id: Number(strId),
+        title,
+      };
+    }),
+  };
+}
+
+const Items = async () => {
+  const data = await getData();
   return (
     <>
-      <Player id={x} />
+      {data.items.map((item) => (
+        <>
+          <div>{item.title}</div>
+          <div>{item.id}</div>
+          <Player id={item.id} />
+        </>
+      ))}
     </>
   );
 };
 
-export default Page;
+export default Items;
