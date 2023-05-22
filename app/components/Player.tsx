a63787d9cbfa675bae26066d37a6d638db1b7edf
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-22 21:22:32 +0200

    feat: player

--- app/components/Player.tsx
+++ app/components/Player.tsx
@@ -2,18 +2,20 @@
 
 interface IPlayer {
   id: number;
+  className?: string;
 }
 
 import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
-const Player = ({ id }: IPlayer) => {
+const Player = ({ id, className }: IPlayer) => {
   return id ? (
     <ReactPlayer
       url={`/items/${id}/video.mp4`}
       loop={true}
       playing
       controls={true}
+      className={className ?? {}}
     />
   ) : null;
 };
