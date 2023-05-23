980c4e10ce1323a70c016a619dd48dd2007652b3
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-23 21:22:32 +0200

    feat: items page

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -3,7 +3,7 @@ import path from "path";
 import { Item } from "@/app/components/types";
 import Player from "@/app/components/Player";
 
-async function getData(): Promise<{ count: number; items: Item[] }> {
+async function getItems(): Promise<{ count: number; items: Item[] }> {
   const itemFolder = path.join(process.cwd(), "public", "items");
   const items = fs.readdirSync(itemFolder);
 
@@ -27,17 +27,22 @@ async function getData(): Promise<{ count: number; items: Item[] }> {
 }
 
 const Items = async () => {
-  const data = await getData();
+  const data = await getItems();
   return (
-    <>
+    <div className="container mx-auto">
       {data.items.map((item) => (
-        <>
-          <div>{item.title}</div>
-          <div>{item.id}</div>
-          <Player id={item.id} />
-        </>
+        <div
+          key={`item-line-${item.id}`}
+          className="my-2 p-2 border-b-2 border-slate-500"
+        >
+          <div className="text-yellow-200 text-xs">#{item.id}</div>
+          <div className="text-green-400 text-xl">{item.title}</div>
+          <div className="my-2">
+            <Player id={item.id} className="mx-auto" />
+          </div>
+        </div>
       ))}
-    </>
+    </div>
   );
 };
 
