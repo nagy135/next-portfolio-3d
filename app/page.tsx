57ac05684a24b33c055c741b6990433a1af7b45f
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-25 08:51:45 +0200

    feat: font, index page start

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import path from "path";
 import fs from "fs";
+import Link from "next/link";
 
 async function getData() {
   const itemFolder = path.join(process.cwd(), "public", "items");
@@ -13,7 +14,19 @@ export default async function Page() {
 
   return (
     <main>
-      <pre>{JSON.stringify(data)}</pre>
+      <div className="container mx-auto mt-5">
+        <div className="text-7xl w-1/2 mx-auto text-center">Welcome</div>
+        <div className="text-xl w-1/2 mt-10 mx-auto text-center">
+          This is my page to preview my 3d printed models with their video
+          animations and 3d-viewable preview
+        </div>
+        <Link
+          className="border-gray-50 border-2 rounded p-3 text-3xl mx-auto block w-1/3 text-center mt-20"
+          href="/items"
+        >
+          Continue
+        </Link>
+      </div>
     </main>
   );
 }
